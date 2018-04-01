e575234d0b878330ceac88bc6cf0ca3407b47a10
Jean Carlo <jeancarlo.flores@vasslatam.com>
2018-04-01 10:39:14 -0500

    fixed 5

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,3 +1,10 @@
+/** Clasess from udemy
+* These classes contain the JAXB annotations.
+*
+* @since 1.0
+* @author jeancarloflores
+* @version 0.1
+*/
 package com.example;
 /**
  * This is a class.
