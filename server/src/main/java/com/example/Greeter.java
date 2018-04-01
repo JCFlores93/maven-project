f8ef7e06c64c25b3dbbca7f883205f766a6b20eb
Jean Carlo <jeancarlo.flores@vasslatam.com>
2018-04-01 10:42:25 -0500

    fixed 6

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -6,16 +6,19 @@
 * @version 0.1
 */
 package com.example;
+
 /**
  * This is a class.
  */
 public class Greeter {
+
   /**
    * This is a constructor.
    */
   public Greeter() {
 
   }
+  
   /**
    * @param someone the name of a person
    * @return  greeting string
